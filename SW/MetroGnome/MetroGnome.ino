b4f437bd5d07356617a924ccdbf1922ca060317f
Pablo Lago <lagopablo@hotmail.es>
2018-04-12 13:30:37 +0200

    Added interrupt button

--- SW/MetroGnome/MetroGnome.ino
+++ SW/MetroGnome/MetroGnome.ino
@@ -9,28 +9,44 @@
 
 #define LED     8       // LED pin
 #define BUZZER  10      // Buzzer pin
-#define MIN_BPM 20      // minimum bpm value
+#define MIN_BPM 40      // minimum bpm value
 #define MAX_BPM 240     // maximum bpm value
 #define POT A0          // pot analog pin
 
-int bpm;
+bool but_status = 0;    // start or stop variable
+int bpm;                //bpm value
 
 
 void setup() {
   Serial.begin(9600);
   pinMode(LED, OUTPUT);  // LED pin as output
+  attachInterrupt(0, Button, FALLING);  // ISR on pin 2 (INT0) when there is a falling edge
 }
 
 void loop() {
-  bpm = map(analogRead(POT), 0, 1023, MIN_BPM, MAX_BPM);
-  Serial.print(bpm);
-  Serial.print(" BPM");
-  digitalWrite(LED, HIGH);
-  tone(BUZZER, 2000);
-  Serial.print("click");
-  delay(6000/bpm);        // 10% of T (where T is the time between two BPSs)
-  digitalWrite(LED, LOW);
-  noTone(BUZZER);
-  delay(54000 / bpm);     // 90% of T
+  if(but_status ==1){
+    bpm = map(analogRead(POT), 0, 1023, MIN_BPM, MAX_BPM);
+    Serial.print(bpm);
+    Serial.print(" BPM ");
+    digitalWrite(LED, HIGH);
+    tone(BUZZER, 2000);
+    Serial.print("click\n");
+    delay(6000/bpm);        // 10% of T (where T is the time between two BPSs)
+    digitalWrite(LED, LOW);
+    noTone(BUZZER);
+    delay(54000 / bpm);     // 90% of T
+  }
+}
 
+void Button()
+{
+  but_status = !but_status;   // toggle but_status
+  delay(30);                  // waits 30 milliseconds
+  if(but_status == 0)         // if but_status is 0...
+  {
+    Serial.println("Stop");   // prints on serial monitor "Stop"
+    noTone(BUZZER);           // stops generation of sound
+  }    
+  else
+    Serial.println("Started at:");  // prints on serial monitor
 }
