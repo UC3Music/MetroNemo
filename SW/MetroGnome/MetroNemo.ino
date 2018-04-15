d2a8c4e266473c992353acfc54a7507f9525b9eb
Tomás Criado <36898802+MrTommyGuns@users.noreply.github.com>
2018-04-15 22:50:43 +0200

    3 Modes and Tap Tempo +
    3 modes added:
    Mode 0 = Stop/Tap Tempo
    Mode 1 = Tap Tempo
    Mode 2 = Potenciometer Tempo
    
    In the Mode 0, the led and the buzzer turn on when the button is pressed.

--- SW/MetroGnome/MetroNemo.ino
+++ SW/MetroGnome/MetroNemo.ino
@@ -1,5 +1,5 @@
 /*
- * MetroGnome
+ * MetroNemo
  * UC3Music: https://music.uc3m.es
  * Author: Pablo Lago (https://github.com/L4g0)
  * 
@@ -12,32 +12,67 @@
 #define MIN_BPM 40      // minimum bpm value
 #define MAX_BPM 240     // maximum bpm value
 #define POT A0          // pot analog pin
-#define TAP 5           //PIN for the TapTempo
+#define TAP 5           // PIN for the TapTempo
 
 bool but_status = 0;    // start or stop variable
-int bpm;                //bpm value
+bool but_tap_status = 0;// TapTempo button status
+int mode = 0;           // Set 
+int bpm;                // bpm value
 
 ArduinoTapTempo tapTempo; //Arduino TapTempo Object
 
 void setup() {
   Serial.begin(9600);
+
+  Serial.println("Started\nIncicial Tempo 120 bpm\nTap to set a Tempo or press the Mode Button");
   
   pinMode(LED, OUTPUT);  // LED pin as output
   
-  attachInterrupt(0, Button, FALLING);  // ISR on pin 2 (INT0) when there is a falling edge
+  attachInterrupt(digitalPinToInterrupt(2), Button, FALLING);  // ISR on pin 2 (INT0) when there is a falling edge
   
-  pinMode(TAP, INPUT); //Conf button for TapTempo
-  digitalWrite(TAP, HIGH);
+  pinMode(TAP, INPUT); //Conf pin for Tap Tempo button
+  digitalWrite(TAP, HIGH); //Conf pin for Tap Tempo button
 }
 
 void loop() {
-  if(but_status ==1){
-    //bpm = map(analogRead(POT), 0, 1023, MIN_BPM, MAX_BPM);  //Read the Tempo with a Pot
-    
+  if(mode == 1){
     bpm = tapTempo.getBPM();  //Read the Tempo with the "TapTempo"
-    //Serial.print("Tap bpm: ");  //Test the asigned Tempo
     //Serial.println(tapTempo.getBPM());
-    
+    Tempo();
+  }else if(mode == 2){
+    bpm = map(analogRead(POT), 0, 1023, MIN_BPM, MAX_BPM);  //Read the Tempo with a Pot
+    Serial.print("Tap bpm: ");  //Test the asigned Tempo
+    Tempo();
+  }else{
+    but_tap_status = digitalRead(TAP) == LOW; // get the state of the button
+    tapTempo.update(but_tap_status); // update ArduinoTapTempo
+    if(digitalRead(TAP) == HIGH){
+      //Serial.println("TAP!");
+      tone(BUZZER, 1000);
+    }else{;
+      noTone(BUZZER);
+    }
+  }
+}
+
+void Button()
+{
+  but_status = !but_status;  // toggle but_status
+  mode = mode+1;
+  if(mode > 2){ mode = 0;}
+  delay(100);                  // waits 10 milliseconds
+  if(mode == 0)         // Stop And Tap Tempo Mode
+  {
+    Serial.println("Stop\nTap a Tempo!");   // prints on serial monitor "Stop"
+    noTone(BUZZER);           // stops generation of sound
+  }else if(mode == 1){
+    Serial.println("MODE: Tap");  // prints on serial monitor   
+  }else{
+    Serial.println("MODE: Pote");  // prints on serial monitor
+  }
+}
+void Tempo()
+{
     digitalWrite(LED, HIGH);
     tone(BUZZER, 2000);
     Serial.print(bpm);
@@ -46,21 +81,5 @@ void loop() {
     digitalWrite(LED, LOW);
     noTone(BUZZER);
     delay(54000 / bpm);     // 90% of T
-  }else{
-    boolean buttonDown = digitalRead(TAP) == LOW; // get the state of the button
-    tapTempo.update(buttonDown); // update ArduinoTapTempo
-  }
 }
 
-void Button()
-{
-  but_status = !but_status;   // toggle but_status
-  delay(10);                  // waits 10 milliseconds
-  if(but_status == 0)         // if but_status is 0...
-  {
-    Serial.println("Stop");   // prints on serial monitor "Stop"
-    noTone(BUZZER);           // stops generation of sound
-  }    
-  else
-    Serial.println("Started at:");  // prints on serial monitor
-}
